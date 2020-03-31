30962e088a32a0bf352b9691311558297b5287ce
Yasiru Pathirana <p.yasiru@directfn.com>
2020-03-31 10:01:34 +0530

    Added Functionality to take non-normalized data set as well.
    preprocess(filename, bIsNormalized)

--- DataAnalytics.py
+++ DataAnalytics.py
@@ -1,10 +1,20 @@
 import pandas as pd
 import numpy as np
 
+""" 
+    Used Min-Max Standard Normalization Technique to Normalize the data set
+    OriginalValue = (OriginalValue - min(Data Set) )/(max(DataSet) - min(DataSet))
+"""
 
-def preprocess(filename):
+
+def min_max_normalization(dataFrame):
+    normalized_data = (dataFrame - np.min(dataFrame)) / (np.max(dataFrame) - np.min(dataFrame))
+
+    return normalized_data
+
+
+def preprocess(filename, bIsNormalize):
     dataFrame = pd.read_excel(filename, usecols=['Time', 'Open', 'Close', 'Chg', 'PctChg'])
-    print(dataFrame.columns.ravel())
 
     # Drop any empty data points in the excel
     for index, rows in dataFrame.iterrows():
@@ -13,14 +23,16 @@ def preprocess(filename):
                 dataFrame.drop(index, inplace=True)
                 print("Dropped Index: %d", index)
 
-    """ Normalize Data Set """
-    dataFrame.Open = minmaxnormalization(dataFrame.Open)
-    dataFrame.Close = minmaxnormalization(dataFrame.Close)
-    dataFrame.Chg = minmaxnormalization(dataFrame.Chg)
-    dataFrame.PctChg = minmaxnormalization(dataFrame.PctChg)
+    """ 
+    Normalize Data Set if needed
+    """
+    if bIsNormalize:
+        dataFrame.Open = min_max_normalization(dataFrame.Open)
+        dataFrame.Close = min_max_normalization(dataFrame.Close)
+        dataFrame.Chg = min_max_normalization(dataFrame.Chg)
+        dataFrame.PctChg = min_max_normalization(dataFrame.PctChg)
 
-    # print(dataFrame)
-
+    # Insert data to data dictionary
     dataDictionary = {}
     for index, row in dataFrame.iterrows():
         dataDictionary[row['Time']] = datarow = {'Open': row['Open'], 'Close': row['Close'], 'Change': row['Chg'],
@@ -29,15 +41,4 @@ def preprocess(filename):
     return dataDictionary
 
 
-""" 
-    Used Min-Max Standard Normalization Technique to Normalize the data set
-    
-    OriginalValue = (OriginalValue - min(Data Set) )/(max(DataSet) - min(DataSet))
-"""
-def minmaxnormalization(dataFrame):
-    normalized_data = (dataFrame - np.min(dataFrame)) / (np.max(dataFrame) - np.min(dataFrame))
-
-    return normalized_data
-
-
-preprocess('1150.xlsx')
+preprocess('1150.xlsx', False)
